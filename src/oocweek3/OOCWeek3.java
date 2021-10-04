31a1c4c244bf0e59079e993b7f3ce132da00fb87
YWR <yuri.ribeiro@gmail.com>
2021-10-04 12:47:38 +0100

    second commit

--- src/oocweek3/OOCWeek3.java
+++ src/oocweek3/OOCWeek3.java
@@ -16,7 +16,8 @@ public class OOCWeek3 {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        System.out.println("This is my first repository");
+        
+        System.out.println("Second change");
     }
     
 }
