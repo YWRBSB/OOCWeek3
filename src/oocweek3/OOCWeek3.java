57a67974728ae9310e2e4e4e8453511eaabab7c6
YWR <yuri.ribeiro@gmail.com>
2021-10-04 12:45:57 +0100

    new commit
    this is my new class

--- src/oocweek3/OOCWeek3.java
+++ src/oocweek3/OOCWeek3.java
@@ -15,6 +15,8 @@ public class OOCWeek3 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        System.out.println("This is my first repository");
     }
     
 }
